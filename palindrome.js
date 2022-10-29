a5821b2a49425d334384649a7d50d2f4250d7af5
shreekant totla <shreekanttotla3@gmail.com>
2022-10-29 20:18:08 +0530

    palandromic string

---
+++ palindrome.js
@@ -0,0 +1,14 @@
+let str1="";
+let str="naman";
+   for(let i=str.length-1;i>=0;i--)
+    {
+        str1=str[i];
+    }
+    if(str==str1)
+    {
+        console.log("Yes");
+    }
+    else
+    {
+        console.log("No");
+    }
